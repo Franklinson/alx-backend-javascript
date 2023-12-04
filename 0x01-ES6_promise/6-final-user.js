92cebcef4d0e8ea03ab48eed12476a0445197cb6
Franklinson <franklinhassey@gmail.com>
2023-12-04 00:23:45 +0000

    editted

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      const arr = [];
-      values.forEach((val) => {
-        const ob = {};
-        ob.status = val.status;
-        if (val.value) ob.value = val.value;
-        if (val.reason) ob.value = val.reason;
-        arr.push(ob);
-      });
-      return arr;
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
     });
-}
+    return result;
+  });}
