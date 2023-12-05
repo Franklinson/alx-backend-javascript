e55413733c6c54ac144b7402a7b1985f2ea9fe75
Franklinson <franklinhassey@gmail.com>
2023-12-05 21:56:56 +0000

    edited 4th task

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,11 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  // map to update grades for students in the specified city
-  return students.map(student => {
-    if (student.location === city) {
-      const matchingGrade = newGrades.find(grade => grade.studentId === student.id);
-      if (matchingGrade) {
-        // If a matching grade is found, update the student's grade
-        return { ...student, grade: matchingGrade.grade };
-      }
-    }
-    // If the student is not in the specified city or a matching grade is not found, return the original student object
-    return student;
+export default function updateStudentGradeByCity(student, city, newGrades) {
+  const stuByCity = student.filter((obj) => obj.location === city);
+  return stuByCity.map((objstu) => {
+    const obj = objstu;
+    newGrades.forEach((grad) => {
+      if (obj.id === grad.studentId) obj.grade = grad.grade;
+    });
+    if (!obj.grade) obj.grade = 'N/A';
+    return obj;
   });
 }
