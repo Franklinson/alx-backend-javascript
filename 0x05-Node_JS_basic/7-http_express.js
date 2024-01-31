296667a0b7f8c9458d7d4c915ade1554c870f0a8
Franklinson <franklinhassey@gmail.com>
2024-01-31 00:16:00 +0000

    server and database on express js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const path = process.argv[2];
+
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.write('This is the list of our students\n');
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      res.write((new Error('Cannot load the database')).stack);
+      res.end();
+    } else {
+      const ob = {};
+      let student = 0;
+      for (const line of data.split('\n')) {
+        if (line !== '') {
+          const fields = line.split(',');
+          if (fields[3] !== 'field') {
+            if (ob[fields[3]] === undefined) {
+              ob[fields[3]] = [0, []];
+            }
+            ob[fields[3]][0] += 1;
+            student += 1;
+            ob[fields[3]][1].push(fields[0]);
+          }
+        }
+      }
+      res.write(`Number of students: ${student}\n`);
+      for (const key of Object.keys(ob)) {
+        res.write(`Number of students in ${key}: ${ob[key][0]}. \
+List: ${ob[key][1].join(', ')}\n`);
+      }
+      res.end();
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
+
+module.exports = app;
