6c9961d9f1f8a502e849a546735bc2e40c6a6089
Franklinson <franklinhassey@gmail.com>
2024-02-02 08:23:22 +0000

    edited files

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,13 +1,28 @@
-const request = require('request');
-const chai = require('chai');
-const expect = chai.expect;
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-describe('query the root of the server', function () {
-  it('returns the root info', function () {
-    return request('http://localhost:7865', function (err, res, body) {
-      if (err) console.log(err);
-      expect(body).to.equal('Welcome to the payment system');
-      expect(res.statusCode).to.equal(200);
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
-  });
-})
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+    it("check correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
+    });
+});
